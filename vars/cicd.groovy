ec87c52ff8f05c8452efdca51bf115aa58dbb802
tinku2906 <152770792+tinku2906@users.noreply.github.com>
2023-12-19 12:01:31 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -6,4 +6,12 @@ def build()
 {
   sh 'mvn package'
 }
+def deploytamcat(jobname,ip,context)
+{
+    sh "scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/${context}.war"
+}
+def testexec(jobname)
+{
+   sh "java -jar /var/lib/jenkins/workspace/${jobname}/testing.jar"
+}
 
